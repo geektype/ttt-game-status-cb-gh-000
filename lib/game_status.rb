8937aa6e6990bc3d1146d9f82212ac2ce8ede9b8
Abdullah Ahmed <abdullah@softcomnetworks.com>
2019-08-04 16:31:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,7 +44,7 @@ nested_students = [
   ["Tim", "Grade 10", "C average"],
   ["Monique", "Grade 11", "B average", "Class President"]
 ]
- 
+
 nested_students.each do |student_array|
   # #inspect returns a human-readable representation
   # of the array
