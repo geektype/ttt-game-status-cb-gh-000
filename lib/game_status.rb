cad3fa3fd380c7a14177f03cfdb0af11969f7252
Abdullah Ahmed <abdullah@softcomnetworks.com>
2019-08-04 16:31:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,5 +36,17 @@ def won?(board)
   end
 end
 
-board = [" ", " ", " ", " ", " ", " ", "X", "X", "X"]
-puts won?(board)
+# board = [" ", " ", " ", " ", " ", " ", "X", "X", "X"]
+# puts won?(board)
+
+nested_students = [
+  ["Mike", "Grade 10", "A average"],
+  ["Tim", "Grade 10", "C average"],
+  ["Monique", "Grade 11", "B average", "Class President"]
+]
+ 
+nested_students.each do |student_array|
+  # #inspect returns a human-readable representation
+  # of the array
+  puts student_array.inspect
+end
